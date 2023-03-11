c58cfb507e03953d340686541eb76a3efe910913
Debanjan Poddar <debanjan2602@gmail.com>
2023-03-12 01:15:52 +0530

    used custom hook and redux state to build image url

--- src/pages/Home/HeroBanner/HeroBanner.jsx
+++ src/pages/Home/HeroBanner/HeroBanner.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./HeroBanner.scss";
+import Img from "../../../components/LazyLoadImage/Img";
+import ContentWrapper from "../../../components/ContentWrapper/ContentWrapper";
+
+import useFetch from "../../../hooks/useFetch";
 
 const HeroBanner = () => {
     const [background, setBackground] = useState("");
     const [query, setQuery] = useState("");
 
     const navigate = useNavigate();
+    const { url } = useSelector(state => state.home);
+
+    const { data, loading } = useFetch("/movie/upcoming");
+
+    useEffect(() => {
+        const bg =
+            url.backdrop +
+            data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+        setBackground(bg);
+    }, [data]);
 
     const searchQueryHandler = e => {
         if (e.key === "Enter" && query.length > 0) {
@@ -17,7 +32,13 @@ const HeroBanner = () => {
 
     return (
         <div className="heroBanner">
-            <div className="wrapper">
+            {!loading && (
+                <div className="backdrop-img">
+                    <Img src={background} />
+                </div>
+            )}
+            <div className="opacity-layer"></div>
+            <ContentWrapper>
                 <div className="heroBannerContent">
                     <span className="title">Welcome</span>
                     <span className="subTitle">
@@ -34,7 +55,7 @@ const HeroBanner = () => {
                         <button>Search</button>
                     </div>
                 </div>
-            </div>
+            </ContentWrapper>
         </div>
     );
 };
